fbce3233f990544a863f0872d55ebd225868da5e
VeryCreativeName64 <marekbendeguz@gmial.com>
2025-09-15 14:10:52 +0200

    header, nav es article kepek mappa

--- admin.php
+++ admin.php
@@ -1,14 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adataim</title>
+    <title id="cim">Adataim</title>
 </head>
 
 <body>
+    <main>
+        <header><img src="kepek/logo_csinfo_v6.gif" alt="csinfo logo"></header>
+        <nav>
+            <a href="#"><h2>Admin rendszer</h2></a>
+            <a href="#"><h2>Csinfo.hu Admin</h2></a>
+            <a href="#"><h2>Adminisztrátor</h2></a>
+        </nav>
+        <nav>
+            <a href="#">Tartalomkezelés</a>
+            <a href="#">Adataim</a>
+            <a href="#">Kilépés</a>  
+        </nav>
+        <article>
 
+        </article>
+    </main>
 </body>
 
 </html>
