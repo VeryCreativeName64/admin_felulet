d33514080fe79eb62a2fb4d3c530ac3e9f367713
VeryCreativeName64 <marekbendeguz@gmial.com>
2025-09-15 14:21:56 +0200

    Adatok modositasa es jelszo modositasa

--- admin.php
+++ admin.php
@@ -11,17 +11,40 @@
     <main>
         <header><img src="kepek/logo_csinfo_v6.gif" alt="csinfo logo"></header>
         <nav>
-            <a href="#"><h2>Admin rendszer</h2></a>
-            <a href="#"><h2>Csinfo.hu Admin</h2></a>
-            <a href="#"><h2>Adminisztrátor</h2></a>
+            <a href="#">
+                <h2>Admin rendszer</h2>
+            </a>
+            <a href="#">
+                <h2>Csinfo.hu Admin</h2>
+            </a>
+            <a href="#">
+                <h2>Adminisztrátor</h2>
+            </a>
         </nav>
         <nav>
             <a href="#">Tartalomkezelés</a>
             <a href="#">Adataim</a>
-            <a href="#">Kilépés</a>  
+            <a href="#">Kilépés</a>
         </nav>
         <article>
-
+            <div class="adat">
+                <h2>Adatok módosítása</h2>
+                <label for="fname">Név:</label>
+                <input type="text" id="fname" name="fname">
+                <label for="fname">Email:</label>
+                <input type="text" id="fname" name="fname">
+                <button>Adatok módosítása</button>
+            </div>
+            <div class="jelszo">
+                <h2>Jelszó módosítása</h2>
+                <label for="fname">Régi:</label>
+                <input type="text" id="fname" name="fname">
+                <label for="fname">Új:</label>
+                <input type="text" id="fname" name="fname">
+                <label for="fname">Új megint:</label>
+                <input type="text" id="fname" name="fname">
+                <button>Jelszó módosítása</button>
+            </div>
         </article>
     </main>
 </body>
