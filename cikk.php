46360df60e428cf336a2b7df31945420e61feaaa
VeryCreativeName64 <marekbendeguz@gmial.com>
2025-09-22 14:29:54 +0200

    tablazat letrehozva

--- cikk.php
+++ cikk.php
@@ -48,7 +48,7 @@
                 <button id="nyelv" type="submit">Nyelv kiválasztása</button>
                 <p>További szűréseket állíthatsz be egy rovat vagy cím szerint.</p>
                 <label for="rovat">Rovat:</label>
-                <select name="lang" id="lang">
+                <select name="rovat" id="rovat">
                     <option value="mrovat">--- Minden rovat ---</option>
                 </select>
                 <label for="cimr">Cím(részlet):</label>
@@ -56,6 +56,29 @@
                 <button id="szures" type="submit">Szűrés</button>
                 <p><a href="#">új cikk</a></p>
             </div>
+            <table>
+                <tr>
+                    <th>Létrehozó</th>
+                    <th>Dátum</th>
+                    <th>Nyelv</th>
+                    <th>Rovat</th>
+                    <th>Cím</th>
+                </tr>
+                <tr>
+                    <td>CSINFO.HU Admin</td>
+                    <td>2025.08.14 11:14</td>
+                    <td>HUN</td>
+                    <td>ENG desktop virt</td>
+                    <td><a id="tablink1" href="#">VMware 9 szoftverek</a></td>
+                </tr>
+                <tr> 
+                    <td>CSINFO.HU Admin</td>
+                    <td>2025.08.12 08:31</td>
+                    <td>HUN</td>
+                    <td>box2</td>
+                    <td><a id="tablink2" href="#">A vSphere 7 általános támogatása megszűnik</a></td>
+                </tr>
+            </table>
         </article>
     </main>
 </body>
