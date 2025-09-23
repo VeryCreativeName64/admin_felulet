f24493ed9f207c28573f8a8eb3fd3b5df988c077
VeryCreativeName64 <marekbendeguz@gmial.com>
2025-09-23 14:27:29 +0200

    menupont.php letrehozva, felulet elkezdve

---
+++ menupont.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title id="cim">Menüpontok</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <header><img src="kepek/logo_csinfo_v6.gif" alt="csinfo logo"></header>
+        <nav class="felso">
+            <h4>Admin rendszer</h4>
+            <h4>Csinfo.hu Admin</h4>
+            <h4>Adminisztrátor</h4>
+        </nav>
+        <nav class="also">
+            <a href="#">Tartalomkezelés</a>
+            <a href="#">Adataim</a>
+            <a href="#">Kilépés</a>
+        </nav>
+        <nav class="alsobb">
+            <a href="#">Alapbeállítások</a>
+            <a href="#">Menüpontok</a>
+            <a href="#">Rovatok</a>
+            <a href="#">Cikkek</a>
+        </nav>
+        <article>
+            <div class="mpont">
+                <label for="lang">Nyelv:</label>
+                <select name="lang" id="lang">
+                    <option value="hun">HUN</option>
+                    <option value="eng">ENG</option>
+                    <option value="ger">GER</option>
+                    <option value="fra">FRA</option>
+                </select>
+                <button id="nyelv" type="submit">Nyelv beállítása</button>
+                <p><a href="#">Új a gyökérbe</a></p>
+
+                <div class="fmenu">
+                    <h4>Felső menü</h4>
+                    <p id="a">Felső menüszerkezet lemásolása másik nyelvre</p>
+                    <p>
+                        Ezt a teljes menüszerkezetet lemásolhatod egy másik nyelvre. Utána természetesen az egyes pontokat át kell
+                        írni az adott nyelvnek megfelelően, bővítheted vagy törölhetsz belőle menüket.
+                        Ha "Cikkeket is másol" kapcsolót bejelölöd, akkor nem csak a menüszerkezetet, hanem az azokhoz rendelt összes
+                        cikk is lemásolódik a cél nyelvre, azonos tartalommal, így utána azokat is módosítanod kell.
+                    </p>
+                    <select title="amenu" id="amenu">
+                        <option value="amenu">--- Válassz célnyelvet! ---</option>
+                    </select>
+                    <label for="chamenu"> Cikkeket is másol</label>
+                    <input type="checkbox" id="chamenu" name="chamenu" value="Cikkek">
+                    <button id="meenu" type="submit">Menü struktúra másolása</button>
+                </div>
+                <h4>Legfelső menü</h4>
+                <div class="lfmenu">
+                    <p id="b">Legfelső menüszerkezet lemásolása másik nyelvre</p>
+                    <p>
+                        Ezt a teljes menüszerkezetet lemásolhatod egy másik nyelvre. Utána természetesen az egyes pontokat át kell
+                        írni az adott nyelvnek megfelelően, bővítheted vagy törölhetsz belőle menüket.
+                        Ha "Cikkeket is másol" kapcsolót bejelölöd, akkor nem csak a menüszerkezetet, hanem az azokhoz rendelt összes
+                        cikk is lemásolódik a cél nyelvre, azonos tartalommal, így utána azokat is módosítanod kell.
+                    </p>
+                    <select title="bmenu" id="bmenu">
+                        <option value="bmenu">--- Válassz célnyelvet ---</option>
+                    </select>
+                    <label for="chamenu2"> Cikkeket is másol</label>
+                    <input type="checkbox" id="chamenu2" name="chamenu2" value="Cikkek">
+                    <button id="struktura" type="submit">Menü struktúra másolása</button>
+                </div>
+                <h4>Alsó menü</h4>
+                <div class="almenu">
+                    <p id="c">Alsó menüszerkezet lemásolása másik nyelvre</p>
+                    <p>
+                        Ezt a teljes menüszerkezetet lemásolhatod egy másik nyelvre. Utána természetesen az egyes pontokat át kell
+                        írni az adott nyelvnek megfelelően, bővítheted vagy törölhetsz belőle menüket.
+                        Ha "Cikkeket is másol" kapcsolót bejelölöd, akkor nem csak a menüszerkezetet, hanem az azokhoz rendelt összes
+                        cikk is lemásolódik a cél nyelvre, azonos tartalommal, így utána azokat is módosítanod kell.
+                    </p>
+                    <select title="cmenu" id="cmenu">
+                        <option value="cmenu">--- Válassz célnyelvet ---</option>
+                    </select>
+                    <label for="chamenu3"> Cikkeket is másol</label>
+                    <input type="checkbox" id="chamenu3" name="chamenu3" value="Cikkek">
+                    <button id="masolasa" type="submit">Menü struktúra másolása</button>
+                </div>
+            </div>
+        </article>
+    </main>
+</body>
+
+</html>
